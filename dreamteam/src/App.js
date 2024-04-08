ee500e40cce8b121e39e2cc0e9adcf7e8b28de67
Maxwell Weaver <mjweaver88@sccsc.edu>
2024-04-08 12:33:30 -0400

    completed initial user login page

--- dreamteam/src/App.js
+++ dreamteam/src/App.js
@@ -2,59 +2,72 @@ import './App.css';
 import * as React from 'react';
 import { Card, CardActions, CardContent, Button, Typography, TextField, Stack } from '@mui/material';
 
-function App()
- {
+function App() {
   return (
     <div className="App">
+      <Typography variant="h1" gutterBottom>
+        Welcome to Sports Search
+      </Typography>
       <header className="App-header">
-      <Typography
-      sx={{fontSize: 8}}
-      >Welcome to SportsSearch</Typography>
-    <Card sx={{ minWidth: 300, minHeight: 100, transform: 'scale(3.5)' }} 
-          variant="outlined">
-      <CardContent>
-      <Stack
-      component="form"
-      sx={{width: '15ch', height: '20'}}
-      spacing={3}
-      noValidate
-      autoComplete="off"
-      >
-      <TextField
-        label="Username"
-        id="outlined-size-small"
-        size="small"
-        variant="outlined" 
-      />
-      <TextField
-         label="Password"
-         id="outlined-size-small"
-         size="small"
-         variant="outlined" 
-      />
-    </Stack>
-      </CardContent>
-      <CardActions>
-      <Button 
-          sx={{fontSize: 8}}
-          variant="text"
-          onClick={() =>
-          {
-            alert('clicked');
-          }}
-        >Login</Button>
-        <Button
-         sx={{fontSize: 8}}
-         variant="text"
-         onClick={() =>
-         {
-           alert('clicked');
-         }}
-        >Create Account</Button>
-      </CardActions>
-    </Card>
+        <div className="card-container">
+          <Card variant="outlined" sx={{ width: '600px', height: '600px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <CardContent>
+            <Typography 
+              variant="button" 
+              display="block"
+              sx={{ textAlign: 'center', marginTop: '10px', marginBottom: '60px', fontSize: 35 }}
+               gutterBottom>
+              LOGIN
+            </Typography>
+              <Stack
+                component="form"
+                sx={{ width: '100%', textAlign: 'center' }}
+                spacing={3}
+                noValidate
+                autoComplete="off"
+              >
+                <TextField
+                  label="Username"
+                  id="outlined-size-large"
+                  size="large"
+                  variant="outlined"
+                  sx={{width: '400px', fontSize: 2000}}
+                />
+                <TextField
+                  label="Password"
+                  id="outlined-size-large"
+                  size="large"
+                  variant="outlined"
+                  type="password"
+                  sx={{width: '400px'}}
+                />
+                <CardActions> 
+                  <Button
+                    sx={{ fontSize: 15 }}
+                    variant="text"
+                    onClick={() => {
+                      alert('Login clicked');
+                    }}
+                  >
+                    Login
+                  </Button>
+                  <Button
+                    sx={{ fontSize: 15 }}
+                    variant="text"
+                    onClick={() => {
+                      alert('Create Account clicked');
+                    }}
+                  >
+                    Create Account
+                  </Button>
+                </CardActions>
+              </Stack>
+            </CardContent>
+          </Card>
+        </div>
       </header>
     </div>
   );
 }
+
 export default App;
