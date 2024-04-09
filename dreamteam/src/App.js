e8c15112e89dfd9b74e6a06e9aefd1d29b4acbca
Maxwell Weaver <mjweaver88@sccsc.edu>
2024-04-08 22:16:21 -0400

    created a 'create account' page

--- dreamteam/src/App.js
+++ dreamteam/src/App.js
@@ -2,27 +2,31 @@ import './App.css';
 import * as React from 'react';
 import { Card, CardActions, CardContent, Button, Typography, TextField, Stack } from '@mui/material';
 
-function App() {
+function LoginPage({ setPage }) {
+  const handleCreateAccountClick = () => {
+    setPage('createAccount');
+  };
+
   return (
     <div className="App">
       <Typography variant="h1" gutterBottom>
-        Welcome to Sports Search
+        Welcome to Sports Search!
       </Typography>
       <header className="App-header">
         <div className="card-container">
           <Card variant="outlined" sx={{ width: '600px', height: '600px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <CardContent>
-            <Typography 
-              variant="button" 
-              display="block"
-              sx={{ textAlign: 'center', marginTop: '10px', marginBottom: '60px', fontSize: 35 }}
-               gutterBottom>
-              LOGIN
-            </Typography>
+              <Typography 
+                variant="button" 
+                display="block"
+                sx={{ textAlign: 'center', marginTop: '10px', marginBottom: '60px', fontSize: 35 }}
+                gutterBottom>
+                LOGIN
+              </Typography>
               <Stack
                 component="form"
                 sx={{ width: '100%', textAlign: 'center' }}
-                spacing={3}
+                spacing={5}
                 noValidate
                 autoComplete="off"
               >
@@ -31,7 +35,7 @@ function App() {
                   id="outlined-size-large"
                   size="large"
                   variant="outlined"
-                  sx={{width: '400px', fontSize: 2000}}
+                  sx={{ width: '400px' }}
                 />
                 <TextField
                   label="Password"
@@ -39,7 +43,7 @@ function App() {
                   size="large"
                   variant="outlined"
                   type="password"
-                  sx={{width: '400px'}}
+                  sx={{ width: '400px' }}
                 />
                 <CardActions> 
                   <Button
@@ -54,9 +58,7 @@ function App() {
                   <Button
                     sx={{ fontSize: 15 }}
                     variant="text"
-                    onClick={() => {
-                      alert('Create Account clicked');
-                    }}
+                    onClick={handleCreateAccountClick}
                   >
                     Create Account
                   </Button>
@@ -66,6 +68,89 @@ function App() {
           </Card>
         </div>
       </header>
+    </div>
+  );
+}
+
+function CreateAccountPage({ setPage }) {
+  const handleBackClick = () => {
+    setPage('login');
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="card-container">
+          <Card variant="outlined" sx={{ width: '600px', height: '600px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <CardContent>
+              <Typography 
+                variant="button" 
+                display="block"
+                sx={{ textAlign: 'center', marginTop: '10px', marginBottom: '60px', fontSize: 35 }}
+                gutterBottom>
+                Create Account
+              </Typography>
+              <Stack
+                component="form"
+                sx={{ width: '100%', textAlign: 'center' }}
+                spacing={5}
+                noValidate
+                autoComplete="off"
+              >
+                <TextField
+                  label="Create Username"
+                  id="outlined-size-large"
+                  size="large"
+                  variant="outlined"
+                  sx={{ width: '400px' }}
+                />
+                <TextField
+                  label="Create Password"
+                  id="outlined-size-large"
+                  size="large"
+                  variant="outlined"
+                  type="password"
+                  sx={{ width: '400px' }}
+                />
+                <TextField
+                  label="Confirm Password"
+                  id="outlined-size-large"
+                  size="large"
+                  variant="outlined"
+                  type="password"
+                  sx={{ width: '400px' }}
+                />
+                <CardActions> 
+                  <Button
+                    sx={{ fontSize: 15 }}
+                    variant="text"
+                    onClick={handleBackClick}
+                  >
+                    Back to Login
+                  </Button>
+                  <Button
+                    sx={{ fontSize: 15 }}
+                    variant="text"
+                  >
+                    Create Account
+                  </Button>
+                </CardActions>
+              </Stack>
+            </CardContent>
+          </Card>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+function App() {
+  const [page, setPage] = React.useState('login');
+
+  return (
+    <div className="App">
+      {page === 'login' && <LoginPage setPage={setPage} />}
+      {page === 'createAccount' && <CreateAccountPage setPage={setPage} />}
     </div>
   );
 }
