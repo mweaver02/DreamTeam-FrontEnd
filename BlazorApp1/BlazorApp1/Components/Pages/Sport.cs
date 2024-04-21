a2445c014435b674465c1efe49ba99a57ee59d2b
Aaron Rash <155991638+Aaron-Rash@users.noreply.github.com>
2024-04-20 23:23:13 -0400

    Added search method and sport class

---
+++ BlazorApp1/BlazorApp1/Components/Pages/Sport.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+using System.Data.OleDb;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace BlazorApp1.Components.Pages
+{
+    public class Sport
+    {
+
+        public OleDbConnection Connection;
+        public string Table;
+        public string Column;
+        string query;
+
+        public Sport(string connectionString, string defaultTable) 
+        { 
+            Connection = new OleDbConnection(connectionString);
+            Connection.Open();
+            Table = defaultTable;
+        }
+
+        public DataTable Search(string column, string text)
+        {
+            if (column == "")
+            {
+                query = $"SELECT * FROM {Table}";
+            }
+            else
+            {
+                query = $"SELECT * FROM {Table} WHERE [{column}] LIKE @search"; //Put columns in brackets or sql gets angry
+            }
+            OleDbCommand command = new OleDbCommand(query, Connection);
+            OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+            adapter.SelectCommand.Parameters.Clear();
+            adapter.SelectCommand.Parameters.AddWithValue("@search", $"{text}%");
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            return dataTable;
+        }
+
+    }
+}
--- BlazorApp1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04d49e6139942a24fa126da26a1656c7fce7e314")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad38b71b4069c211ef9f0c52caf1c581c81a8ed7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
