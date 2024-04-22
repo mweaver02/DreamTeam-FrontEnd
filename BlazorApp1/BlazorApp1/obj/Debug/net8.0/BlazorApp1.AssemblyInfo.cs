703e40f74ea517318169b90b34476a922b20c3b8
Aaron Rash <155991638+Aaron-Rash@users.noreply.github.com>
2024-04-21 20:03:58 -0400

    visual perfection

--- BlazorApp1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+388ecab1215a1065dd6dadcd4b3488c06c37bccf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c93f7098abf733391f78b51feacb23c85611bec")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
